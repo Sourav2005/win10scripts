d04896ecf81eec40efa52b37b65a25dae63063e7
I am L <61320974+Sourav2005@users.noreply.github.com>
2021-09-15 21:56:55 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -103,6 +103,7 @@ $wingetapps = @(
 	"Github Desktop")
 
 # Chocolatey
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco feature enable -n allowGlobalConfirmation
 choco install -y git -params '"/GitAndUnixToolsOnPath /WindowsTerminal"'
 foreach ($chocoapp in $chocoapps) {
@@ -219,9 +220,84 @@ Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-L
 Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
 $restart = Read-Host "Do you want to restart your pc?"
 if($restart -eq 'yes'){
-   Restart-Computer -Force
-   wsl --update
-   wsl --set-default-version 2
+	# Temp Folder
+	if (!(Get-Item C:\temp -ea ignore)) { mkdir C:\temp }
+
+	$dropperscript = 'C:\temp\dropper.ps1'
+
+	$dropper = @'
+	#############################################
+	###        Configuration Variables        ###
+												#
+	# Put any variables you'll use here
+												# 
+	###                                       ###
+	#############################################
+
+	# Static Variables
+	$countfile = 'C:\temp\bootcount.txt'
+	$bootbatch = 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\dropper.bat'
+	$dropperscript = 'C:\temp\dropper.ps1'
+
+	#################
+	##### Setup #####
+
+	# Bootstrap Batch
+	if (!(Get-Item $bootbatch -ea ignore)) {
+		"powershell -c $dropperscript`npause" | Out-File $bootbatch -Encoding 'OEM'
+	}
+
+	# Boot Count
+	if (Get-Item $countfile -ea ignore) {
+		[int]$bootcount = Get-Content $countfile
+		if ($bootcount -match "^\d{1,2}$") { ([int]$bootcount) ++ }
+		else { $bootcount = 1 }
+	}
+	else { $bootcount = 1 }
+	$bootcount | Out-File $countfile
+
+
+	switch ($bootcount) {
+		
+		1 {
+			# Fill in anything needed on first run
+			
+			Restart-Computer
+			##################################################
+			###############     --REBOOT--     ###############
+		}
+		
+		2 {
+			# Fill in anything needed on second reboot; remove if unneeded
+				
+			Restart-Computer
+			##################################################
+			###############     --REBOOT--     ###############
+		}
+		
+		3 {
+			# Fill in anything needed on third reboot; remove if unneeded
+			# Create more reboots as needed
+			
+			Restart-Computer
+			##################################################
+			###############      --END--      ################
+		}
+		
+		default {
+			# Dropper is complete; clean up
+			rm $countfile
+			rm $bootbatch
+			rm $dropperscript
+		}
+	}
+	'@
+
+	# Drop and run Dropper
+
+	$dropper | Out-File $dropperscript -Encoding 'OEM'
+
+	Invoke-Expression $dropperscript
 }else {
   exit
 }
